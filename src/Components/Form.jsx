b608355c140a453f3c285f2fae9d7c9d700f1bec
shaiksajidhussain <sanjusazid0@gmail.com>
2024-07-24 11:45:25 +0530

    added the test

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -63,7 +63,7 @@ const Form = () => {
     e.preventDefault();
     try {
       // const response = await axios.post('http://localhost:3000/api/feedback', formData, {
-        const response = await axios.post('https://sglbk.vercejb;fsdsflsadfllsdfguis;apgfp;sdl.app/api/feedback', formData, {
+        const response = await axios.post('https://sglbk.vercel.app/api/feedback', formData, {
           headers: {
             'Content-Type': 'application/json',
           },
